2d3bdcc00af3c8d63f4e8de2d457411123e5a9e2
Keivan Beigi <kay.one@gmail.com>
2012-08-24 14:23:43 -0700

    Added basic PolicyService Functions.

--- LivePolicy/LivePolicy.Tests/Class1.cs
+++ LivePolicy/LivePolicy.Tests/Class1.cs
@@ -1,11 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using FluentAssertions;
+using Kayone.TestFoundation;
+using Moq;
+using NUnit.Framework;
 
 namespace LivePolicy.Tests
 {
-    public class Class1
+    [TestFixture]
+    public class PolicyServiceFixture : TestCore<PolicyService>
     {
+
+        private PolicyInfo fakePolicy;
+        private PolicyService _policyService;
+
+        [SetUp]
+        public void Setup()
+        {
+            fakePolicy = new PolicyInfo();
+
+            _policyService = Mocker.Resolve<PolicyService>();
+        }
+
+        protected override PolicyService Subject
+        {
+            get
+            {
+                return _policyService;
+            }
+
+        }
+
+        private void GiveUnavilableSource()
+        {
+            Mocker.GetMock<IPolicySource>()
+                .Setup(c => c.FetchPolicy()).Throws<WebException>();
+        }
+
+        private void GiveSourcePolicy(PolicyInfo policyInfo)
+        {
+            Mocker.GetMock<IPolicySource>()
+                  .Setup(c => c.FetchPolicy()).Returns(policyInfo);
+        }
+
+        private void GiveStoredPolicy(PolicyInfo policyInfo)
+        {
+            Mocker.GetMock<IPolicyStorage>()
+                  .Setup(c => c.Read()).Returns(policyInfo);
+        }
+
+        [Test]
+        public void load_should_readpolicy_from_source_and_store_to_storage()
+        {
+            GiveSourcePolicy(fakePolicy);
+
+            Subject.Load();
+            Subject.Current.Should().Equal(fakePolicy);
+
+            Mocker.GetMock<IPolicyStorage>().Verify(c => c.Write(fakePolicy), Times.Once());
+            Mocker.GetMock<IPolicySource>().Verify(c => c.FetchPolicy(), Times.Once());
+        }
+
+        [Test]
+        public void if_remote_load_failes_stored_policy_should_be_used()
+        {
+            GiveUnavilableSource();
+            GiveStoredPolicy(fakePolicy);
+
+            Subject.Load();
+
+            
+
+            Mocker.GetMock<IPolicyStorage>().Verify(c => c.Write(It.IsAny<PolicyInfo>()), Times.Never());
+            Subject.Current.Should().Equal(fakePolicy);
+        }
+
+
     }
 }
--- LivePolicy/LivePolicy/Class1.cs
+++ LivePolicy/LivePolicy/Class1.cs
@@ -1,11 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace LivePolicy
 {
-    public class Class1
+    public class PolicyInfo : Dictionary<string, string>
     {
+        public DateTime PublishDate { get; set; }
+        public int Version { get; set; }
+    }
+
+    public interface IPolicySource
+    {
+        PolicyInfo FetchPolicy();
+    }
+
+    public interface IPolicyStorage
+    {
+        PolicyInfo Read();
+        void Write(PolicyInfo policyInfo);
+    }
+
+    public class PolicyService
+    {
+        private readonly IPolicySource _policySource;
+        private readonly IPolicyStorage _policyStorage;
+
+        public PolicyService(IPolicySource policySource, IPolicyStorage policyStorage)
+        {
+            _policySource = policySource;
+            _policyStorage = policyStorage;
+        }
+
+        public void Load()
+        {
+            PolicyInfo newPolicy;
+
+            if (TryFetchPolicyFromSource(out newPolicy))
+            {
+                _policyStorage.Write(newPolicy);
+            }
+            else
+            {
+                newPolicy = _policyStorage.Read();
+            }
+
+            Current = newPolicy;
+        }
+
+        private bool TryFetchPolicyFromSource(out PolicyInfo sourcePolicy)
+        {
+            sourcePolicy = null;
+
+            try
+            {
+                sourcePolicy = _policySource.FetchPolicy();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public PolicyInfo Current { get; private set; }
     }
 }
