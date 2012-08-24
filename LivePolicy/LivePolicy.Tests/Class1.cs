e327a1dad77b4821f0f95fc24328b510e7149b1f
Keivan Beigi <kay.one@gmail.com>
2012-08-24 13:41:33 -0700

    Initial commit of solution structure.

---
+++ LivePolicy/LivePolicy.Tests/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LivePolicy.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ LivePolicy/LivePolicy/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LivePolicy
+{
+    public class Class1
+    {
+    }
+}
